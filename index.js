c6717b4730202cc59d8b1373a7b042c66e303f1a
United States CA <jamalhoseen@Jamal-Hoseens-Air.local>
2022-03-12 13:04:34 -0800

    adding places view with mock data

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express()
 app.set('view engine', 'jsx')
 app.engine('jsx', require ('express-react-views').createEngine())
 
-app.use('./places', require('./controllers/places'))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('home')
+    res.send('HOME')
 })
 
 
